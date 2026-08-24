0e6dcee7dab567165d772f5b9a85754e815d7bae
AissaniMourad <aissanimourad444@gmail.com>
2026-08-24 12:21:40 +0100

    Day 7 - Javascript Add & Change HTML 2

---
+++ Exercise13/index13.js
@@ -0,0 +1,23 @@
+// STEP 1 CREATE THE ELEMENT
+const newListItem = document.createElement("li");
+
+// STEP 2 ADD ATTRIBUTES/PROPERTIES
+newListItem.textContent = "coconut";
+newListItem.id = "coconut";
+newListItem.style.fontWeight = "bold";
+newListItem.style.backgroundColor = "lightgreen";
+
+// STEP 3 APPEND ELEMENT TO DOM
+// document.body.append(newListItem);
+// document.body.prepend(newListItem);
+// document.getElementById("fruits").append(newListItem);
+// document.getElementById("fruits").prepend(newListItem);
+
+// const banana = document.getElementById("banana");
+// document.getElementById("fruits").insertBefore(newListItem, banana);
+
+const listItems = document.querySelectorAll("#fruits li");
+document.getElementById("fruits").insertBefore(newListItem, listItems[2]);// if there is no id's for the elements
+
+// STEP 4 REMOVE HTML ELEMENT
+document.getElementById("fruits").removeChild(newListItem);
